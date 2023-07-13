56388a6503c7c2bc237fc57e88a440a280865143
Ling Guo <ling.guo@rea-group.com>
2023-07-13 08:23:08 +0800

    ts rest: add post products api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import { Product } from "./product";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 const port = 3000;
 
-app.get('/hello', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+const products: Product[] = [];
+
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello, World!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.post("/products", (req: Request, res: Response) => {
+  // the request body will be parsed by the `body-parser` middleware
+  console.log(`request body = ${JSON.stringify(req.body)}`);
+  const { id, name, description, brand, country_id } = req.body;
+  const newProduct: Product = { id, name, description, brand, country_id };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
 });
---
+++ src/product.ts
@@ -0,0 +1,9 @@
+import internal from "stream";
+
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    country_id: number;
+}
